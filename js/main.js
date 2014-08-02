30856944f1c2dfa248fa644b0adefae8c7a8c29c
Kate Bankert <kbankert@gmail.com>
2014-08-02 14:46:13 -0700

    push files

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// $( document ).ready(function() {
+//     $(".me td").mouseenter(function() {
+//       $(".info", this).show();
+//       $("img", this).hide();
+//     }).mouseleave(function() {
+//       $(".info", this).hide();
+//       $("img", this).show();
+//     });
+// });
+
+// $(document).ready(function() {
+//     $('a[href=#top]').click(function(){
+//         $('html, body').animate({scrollTop:0}, 'slow');
+//         return false;
+//     });
+// })
